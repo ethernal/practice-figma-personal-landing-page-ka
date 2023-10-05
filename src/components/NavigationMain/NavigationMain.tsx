37403580c732fcfd9c03006dcb70d8e69c0bcb08
Sebastian Pieczynski <testament777@gmail.com>
2023-10-05 17:07:18 +0200

    feat: desktop navigation

---
+++ src/components/NavigationMain/NavigationMain.tsx
@@ -0,0 +1,64 @@
+'use client';
+import * as React from 'react';
+
+import Logo from '@/components/Logo';
+import {
+	Button,
+	Divider,
+	Link,
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	VisuallyHidden,
+} from '@nextui-org/react';
+
+function NavigationMain() {
+	return (
+		<>
+			<Navbar className="justify-between text-lg">
+				<NavbarBrand>
+					<Logo className="w-10 me-2 rounded-full" />
+					<VisuallyHidden className="font-bold text-inherit">
+						KuroiArt
+					</VisuallyHidden>
+				</NavbarBrand>
+				<NavbarContent className="hidden sm:flex gap-12">
+					<NavbarItem>
+						<Link className="text-theme-white" href="#">
+							Home
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Link href="#" className="text-theme-white" aria-current="page">
+							About me
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Link className="text-theme-white" href="#">
+							Works
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Link href="#" className="text-theme-white">
+							Blog
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Button
+							as={Link}
+							className="text-theme-white bg-theme-accent rounded-md shadow-lg px-12 py-3"
+							href="#"
+							variant="flat"
+						>
+							Get in Touch
+						</Button>
+					</NavbarItem>
+				</NavbarContent>
+			</Navbar>
+			<Divider />
+		</>
+	);
+}
+
+export default NavigationMain;
