65d640f0cd1eea9b5a52c0908b3175a18eeabee8
Sebastian Pieczynski <testament777@gmail.com>
2023-10-05 17:46:37 +0200

    refactor: use shadow instead of divider

--- src/components/NavigationMain/NavigationMain.tsx
+++ src/components/NavigationMain/NavigationMain.tsx
@@ -15,49 +15,46 @@ import {
 
 function NavigationMain() {
 	return (
-		<>
-			<Navbar className="justify-between text-lg">
-				<NavbarBrand>
-					<Logo className="w-10 me-2 rounded-full" />
-					<VisuallyHidden className="font-bold text-inherit">
-						KuroiArt
-					</VisuallyHidden>
-				</NavbarBrand>
-				<NavbarContent className="hidden sm:flex gap-12">
-					<NavbarItem>
-						<Link className="text-theme-white" href="#">
-							Home
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link href="#" className="text-theme-white" aria-current="page">
-							About me
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link className="text-theme-white" href="#">
-							Works
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link href="#" className="text-theme-white">
-							Blog
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Button
-							as={Link}
-							className="text-theme-white bg-theme-accent rounded-md shadow-lg px-12 py-3"
-							href="#"
-							variant="flat"
-						>
-							Get in Touch
-						</Button>
-					</NavbarItem>
-				</NavbarContent>
-			</Navbar>
-			<Divider />
-		</>
+		<Navbar className="justify-between text-lg shadow-lg">
+			<NavbarBrand>
+				<Logo className="w-10 me-2 rounded-full" />
+				<VisuallyHidden className="font-bold text-inherit">
+					KuroiArt
+				</VisuallyHidden>
+			</NavbarBrand>
+			<NavbarContent className="hidden sm:flex gap-12">
+				<NavbarItem>
+					<Link className="text-theme-white" href="#">
+						Home
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="#" className="text-theme-white" aria-current="page">
+						About me
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link className="text-theme-white" href="#">
+						Works
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="#" className="text-theme-white">
+						Blog
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Button
+						as={Link}
+						className="text-theme-white bg-theme-accent rounded-md shadow-lg px-12 py-3"
+						href="#"
+						variant="flat"
+					>
+						Get in Touch
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+		</Navbar>
 	);
 }
 
