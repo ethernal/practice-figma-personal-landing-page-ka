89a21e0e4ec54a0c8926fbca2265adb71f54e79e
Sebastian Pieczynski <testament777@gmail.com>
2023-10-06 18:19:23 +0200

    refactor: use extracted Get in Touch button

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -5,12 +5,14 @@ import * as React from 'react';
 
 import { Image } from '@nextui-org/react';
 
+import GetInTouchButton from '../GetInTouchButton';
+
 function HeroSection() {
 	return (
 		<div className="flex justify-center">
 			<div className="grid grid-cols-2">
-				<section className="py-10">
-					<h2 className="text-4xl text-theme-white font-bold pb-4">
+				<section className="flex flex-col gap-10 py-10">
+					<h2 className="text-4xl text-theme-white font-bold">
 						Create your games in{' '}
 						<span className="text-theme-accent">less than 12days</span>
 					</h2>
@@ -21,18 +23,19 @@ function HeroSection() {
 						making! I can design games that are fun to play and highly
 						repayable!
 					</p>
+					<GetInTouchButton className="px-10 py-2 flex-1 self-start" />
 				</section>
 				<section className="relative">
 					<Image
 						src={`/profile/profile-photo.png`}
 						removeWrapper={true}
 						alt="My profile"
-						width={150}
-						height={250}
+						width={250}
+						height={450}
 						className="absolute left-1/4 bottom-0"
 						as={NextImage}
 					/>
-					<div className="absolute w-32 aspect-square bg-[#293241] bottom-10 rounded-full left-1/4"></div>
+					<div className="absolute w-56 aspect-square bg-[#293241]/[0.5] bottom-12 rounded-full right-10"></div>
 				</section>
 			</div>
 		</div>
