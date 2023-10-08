13132a9da3e6e74e67d5f6f973a5e2072fcd62b3
Sebastian Pieczynski <testament777@gmail.com>
2023-10-08 09:00:49 +0200

    feat: add testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import AboutMeSection from '@/components/AboutMeSection';
+import ContentWrapper from '@/components/ContentWrapper';
 import HeroSection from '@/components/HeroSection';
 import NavigationMain from '@/components/NavigationMain';
 import RecentWorkSection from '@/components/RecentWorkSection';
+import TestimonialsSection from '@/components/TestimonialsSection';
 import WorkedForSection from '@/components/WorkedForSection';
 import { SITE_TITLE } from '@/constants/constants';
 
@@ -17,10 +19,12 @@ async function Home() {
 	return (
 		<div className="w-4/5 max-w-7xl">
 			<NavigationMain />
+
 			<HeroSection />
 			<AboutMeSection />
 			<WorkedForSection className="bg-theme-bg-gray-dark" />
 			<RecentWorkSection className="bg-theme-bg-dark" />
+			<TestimonialsSection />
 		</div>
 	);
 }
